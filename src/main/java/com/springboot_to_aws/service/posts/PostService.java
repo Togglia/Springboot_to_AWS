cc67eaf61382d88a574b67c48e47c875bcbbaad7
Hyuk Su <hsoo0720@naver.com>
2023-05-17 19:17:37 +0900

    게시글

--- src/main/java/com/springboot_to_aws/service/posts/PostService.java
+++ src/main/java/com/springboot_to_aws/service/posts/PostService.java
@@ -1,12 +1,17 @@
 package com.springboot_to_aws.service.posts;
 import com.springboot_to_aws.domain.posts.Posts;
 import com.springboot_to_aws.domain.posts.PostsRepository;
+import com.springboot_to_aws.web.dto.PostsListResponseDto;
 import com.springboot_to_aws.web.dto.PostsResponseDto;
 import com.springboot_to_aws.web.dto.PostsSaveRequestDto;
 import com.springboot_to_aws.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RequiredArgsConstructor
 @Service
 public class PostService {
@@ -40,5 +45,12 @@ public class PostService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/com/springboot_to_aws/web/IndexController.java
+++ src/main/java/com/springboot_to_aws/web/IndexController.java
@@ -1,12 +1,19 @@
 package com.springboot_to_aws.web;
 
+import com.springboot_to_aws.service.posts.PostService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
+
+    private final PostService postsService;
+
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("posts",postsService.findAllDesc());
         return "index";
         // 머스테치 스타터 덕분에 컨트롤러에서 문자열 반환 시
         // 앞의 경로와 뒤의 확장자는 자동으로 지정
